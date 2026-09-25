cf77b945049bd3ce119b46037f1c2b313b84a3d8
ls <leonore.jamet@centrale.centralelille.fr>
2026-09-25 11:23:25 +0200

    fix: name error in fibonacci function

--- notebook.py
+++ notebook.py
@@ -4,32 +4,27 @@ __generated_with = "0.24.2"
 app = marimo.App(width="medium")
 
 
-@app.cell
-def _(Fibonnaci):
-    # Fibonacci sequence 
+@app.function
+# Fibonacci sequence 
 
-    def Fibonacci(n) :
-        '''
-        Reminder: the Fibonacci sequence is defined by  
-        F(0) = 0  
-        F(1) = 1   
-        F(n) = F(n−1) + F(n−2)    for n ≥ 2
+def Fibonacci(n) :
+    '''
+    Reminder: the Fibonacci sequence is defined by  
+    F(0) = 0  
+    F(1) = 1   
+    F(n) = F(n−1) + F(n−2)    for n ≥ 2
     
-        '''
-        if (n == 0) :
-            return 0 
-        if (n == 1) : 
-            return 1
-        else : 
-            return (Fibonnaci(n-1) + Fibonacci(n-2))
-            
-            
-
-    return (Fibonacci,)
+    '''
+    if (n == 0) :
+        return 0 
+    if (n == 1) : 
+        return 1
+    else : 
+        return (Fibonacci(n-1) + Fibonacci(n-2))
 
 
 @app.cell
-def _(Fibonacci):
+def _():
     # Tests to pass
 
     assert(Fibonacci(0) == 0)
