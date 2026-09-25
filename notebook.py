1fad4f46c76d2677599c0cb1ab61f28e2989f888
ls <leonore.jamet@centrale.centralelille.fr>
2026-09-25 11:20:32 +0200

    feat: New tests added

--- notebook.py
+++ notebook.py
@@ -4,29 +4,40 @@ __generated_with = "0.24.2"
 app = marimo.App(width="medium")
 
 
-@app.function
-# Fibonacci sequence 
+@app.cell
+def _(Fibonnaci):
+    # Fibonacci sequence 
 
-def Fibonacci(n) :
-    '''
-    Reminder: the Fibonacci sequence is defined by  
-    F(0) = 0  
-    F(1) = 1   
-    F(n) = F(n−1) + F(n−2)    for n ≥ 2
+    def Fibonacci(n) :
+        '''
+        Reminder: the Fibonacci sequence is defined by  
+        F(0) = 0  
+        F(1) = 1   
+        F(n) = F(n−1) + F(n−2)    for n ≥ 2
     
-    '''
+        '''
+        if (n == 0) :
+            return 0 
+        if (n == 1) : 
+            return 1
+        else : 
+            return (Fibonnaci(n-1) + Fibonacci(n-2))
+            
+            
+
+    return (Fibonacci,)
 
 
 @app.cell
-def _():
+def _(Fibonacci):
     # Tests to pass
-    import random 
 
     assert(Fibonacci(0) == 0)
     assert(Fibonacci(1) == 1)
+    assert(Fibonacci(3) == 2)
+    assert(Fibonacci(10) == 55)
 
-    n = random.randint(2,100)
-    assert(Fibonacci(n) == Fibonacci(n-1) + Fibonacci(n-2))
+
 
     return
 
